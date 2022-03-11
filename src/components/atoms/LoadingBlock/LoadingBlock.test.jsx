983ac096c6eb043fa8922044a0c851e8f7641931
leonardohcl <leonardohcl@hotmail.com>
2022-03-11 00:11:59 -0300

    Updates

--- src/components/atoms/LoadingBlock/LoadingBlock.test.jsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import LoadingBlock from "./LoadingBlock";
-
-test("render correctly", () => {
-    const {container} = render(<LoadingBlock />)
-    expect(container.getElementsByClassName("loading-block")).not.toBe(null)
-})
--- src/components/molecules/SearchForm/SearchForm.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import SearchForm from "./SearchForm"
-
-test("render correctly", () => {
-    render(<SearchForm/>)
-    const element = screen.getByPlaceholderText("Type here your search")
-    expect(element).toBeInTheDocument()
-})
